062fdb65fde80d6f1ac6b321d1554786babc3cc3
eduardobnhr <eduardobnhr@gmail.com>
2023-10-11 07:53:54 -0400

    add getter, setter and h2 database

--- src/main/java/br/com/todolistrocketseat/todolist/user/UserModel.java
+++ src/main/java/br/com/todolistrocketseat/todolist/user/UserModel.java
@@ -1,8 +1,24 @@
 package br.com.todolistrocketseat.todolist.user;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
 
     private String username;
     private String name;
     private String password;
+
+
+    
 }
